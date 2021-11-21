68cf248ff57208702d3a637eeb26bc888b19544d
fcventura02 <filipecvr4@gmail.com>
2021-11-20 22:24:18 -0300

    added close menu when redirect to contact

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,9 @@
 const menu = document.getElementById("hamburguer")
+const contato = document.getElementById("link-contato")
+contato.addEventListener("click",()=>{
+	document.getElementById("menu").classList.toggle("hidden")
+	document.getElementById("menu").classList.toggle("block")
+})
 menu.addEventListener("click",()=>{
 	document.getElementById("menu").classList.toggle("hidden")
 	document.getElementById("menu").classList.toggle("block")
